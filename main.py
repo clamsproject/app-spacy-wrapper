220e9ec71173d1c90a99f42803e66fa5bdf0efaf
Kelley Lynch <kelleylynch@gmail.com>
2019-09-26 21:03:47 -0400

    moving to clams/mmif

--- main.py
+++ main.py
@@ -1,74 +1,88 @@
-import os
-import json
-
-from flask import Flask, request
 import spacy
-from lapps import lif
-from lapps.discriminators import Uri
-
-from utils import json_response, JSON_MIME_TYPE
-from metadata import METADATA
+from clams.serve import ClamApp
+from clams.serialize import *
+from clams.vocab import AnnotationTypes
+from clams.vocab import MediaTypes
+from clams.restify import Restifier
+from lapps.discriminators import Uri  # TODO move to clams
 
 # Load English tokenizer, tagger, parser, NER and word vectors
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load("en_core_web_sm")
 
 
-app = Flask(__name__)
+class Spacy(ClamApp):
+    def appmetadata(self):
+        metadata = {
+            "name": "Spacy Wrapper",
+            "description": "This tool applies spacy tools to the transcript.",
+            "vendor": "Team CLAMS",
+            "requires": [MediaTypes.T],
+            "produces": [AnnotationTypes.OCR],
+        }
+        return metadata
+
+    def sniff(self, mmif):
+        # this mock-up method always returns true
+        return True
+
+    def annotate(self, mmif):
+        if type(mmif) is not Mmif:
+            mmif = Mmif(mmif)
+        transcript = ""
+        ##load transcript
+        transcript_location = mmif.get_medium_location(MediaTypes.T)
+        try:
+            with open(transcript_location) as transcript_file:
+                transcript = transcript_file.read()
+        except Exception as e:
+            #TODO handle error
+            print("ERROR", e)
+
+        ##apply spacy to transcript
+        spacy_output = nlp(transcript)
+
+        new_view = mmif.new_view()
+        new_view.new_contain(Uri.TOKEN, "SpacyNLP")
+        new_view.new_contain(Uri.POS, "SpacyNLP")
+        new_view.new_contain(Uri.LEMMA, "SpacyNLP")
+        new_view.new_contain(Uri.NCHUNK, "SpacyNLP")
+        new_view.new_contain(Uri.SENTENCE, "SpacyNLP")
+        new_view.new_contain(Uri.NE, "SpacyNLP")
+        for (n, tok) in enumerate(spacy_output):
+            pos = tok.tag_
+            lemma = tok.lemma_
+            p1 = tok.idx
+            p2 = p1 + len(tok.text)
+            a = new_view.new_annotation(n)
+            a.add_feature("pos", pos)
+            a.add_feature("lemma", lemma)
+            a.add_feature("text", tok.text)
+        # TODO: start and end are not character offsets but token offsets
+        # TODO: need to translate to character offsets or refer to token identifiers
+        for (n, chunk) in enumerate(spacy_output.noun_chunks):
+            a = new_view.new_annotation(n)
+            a.add_feature("start", chunk.start)
+            a.add_feature("end", chunk.end)
+        for (n, sent) in enumerate(spacy_output.sents):
+            a = new_view.new_annotation(n)
+            a.add_feature("start", sent.start)
+            a.add_feature("end", sent.end)
+            a.add_feature("text", sent.text)
+        for (n, ent) in enumerate(spacy_output.ents):
+            a = new_view.new_annotation(n)
+            a.add_feature("start", ent.start)
+            a.add_feature("end", ent.end)
+            a.add_feature("text", ent.text)
+            a.add_feature("category", ent.label_)
+        contain = new_view.new_contain(AnnotationTypes.Tokens)
+        contain.producer = self.__class__
+
+        for contain in new_view.contains.keys():
+            mmif.contains.update({contain: new_view.id})
+        return mmif
 
 
-@app.route("/spacy", methods=['GET'])
-def metadata():
-    return json_response(json.dumps(METADATA))
-
-
-@app.route("/spacy", methods=['POST'])
-def execute():
-    if request.content_type != JSON_MIME_TYPE:
-        error = json.dumps({'error': 'Invalid Content Type'})
-        return json_response(error, 400)
-    doc = nlp(request.json["text"])
-    data = build_data_object(request, doc)
-    return json_response(data.as_pretty_json())
-
-
-def build_data_object(request, doc):
-    #entities = [(ent.start, ent.end) for ent in doc.ents]
-    container = lif.Container()
-    text = request.json["text"]
-    container.set_text(text)
-    container.set_language("en")
-    view = container.new_view("v1")
-    view.add_contains(Uri.TOKEN, "SpacyNLP", "en_core_web_sm")
-    view.add_contains(Uri.POS, "SpacyNLP", "en_core_web_sm")
-    view.add_contains(Uri.LEMMA, "SpacyNLP", "en_core_web_sm")
-    view.add_contains(Uri.NCHUNK, "SpacyNLP", "en_core_web_sm")
-    view.add_contains(Uri.SENTENCE, "SpacyNLP", "en_core_web_sm")
-    view.add_contains(Uri.NE, "SpacyNLP", "en_core_web_sm")
-    for (n, tok) in enumerate(doc):
-        pos = tok.tag_
-        lemma = tok.lemma_
-        p1 = tok.idx
-        p2 = p1 + len(tok.text)
-        a = view.new_annotation("t%d" % (n + 1), Uri.TOKEN, p1, p2)
-        a.add_feature('pos', pos)
-        a.add_feature('lemma', lemma)
-        a.add_feature('text', tok.text)
-    # TODO: start and end are not character offsets but token offsets
-    # TODO: need to translate to character offsets or refer to token identifiers
-    for (n, chunk) in enumerate(doc.noun_chunks):
-        a = view.new_annotation("nc%d" % (n + 1), Uri.NCHUNK, chunk.start, chunk.end)
-    for (n, sent) in enumerate(doc.sents):
-        a = view.new_annotation("s%d" % (n + 1), Uri.SENTENCE, sent.start, sent.end)
-    for (n, ent) in enumerate(doc.ents):
-        a = view.new_annotation("ne%d" % (n + 1), Uri.NE, ent.start, ent.end)
-        a.add_feature('category', ent.label_)
-    data = lif.Data(discriminator=Uri.LIF, payload=container)
-    return data
-
-
-if __name__ == '__main__':
-
-    host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host=host, port=port, debug=True)
-    
+if __name__ == "__main__":
+    spacy_tool = Spacy()
+    spacy_service = Restifier(spacy_tool)
+    spacy_service.run()
